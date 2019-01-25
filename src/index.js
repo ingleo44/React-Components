ff168d16220bda1e28d4e1ea11ace0b6cf92d710
Leonardo Sanchez <leonardosanchez4@hotmail.com>
2019-01-25 13:36:54 -0500

    LS - commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import Faker from 'faker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>Are you sure to do this?
+<CommentDetail author="Sam" timeAgo="Today at 4:45" avatarImage={Faker.image.avatar()} text={Faker.hacker.phrase()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45" avatarImage={Faker.image.avatar()} text={Faker.hacker.phrase()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 2:45" avatarImage={Faker.image.avatar()} text={Faker.hacker.phrase()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Today at 5:15" avatarImage={Faker.image.avatar()} text={Faker.hacker.phrase()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Paul" timeAgo="Today at 4:55" avatarImage={Faker.image.avatar()} text={Faker.hacker.phrase()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Bob" timeAgo="Today at 1:45" avatarImage={Faker.image.avatar()} text={Faker.hacker.phrase()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Maria" timeAgo="Today at 11:45" avatarImage={Faker.image.avatar()} text={Faker.hacker.phrase()} />
+            </ApprovalCard>
+        </div>
+
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
